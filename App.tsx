12e5573b98668817fa7c2dc8ef33502b67c31b03
89catbjo <cathrine.bjornberg@live.se>
2024-09-24 16:26:38 +0200

    homescreen works

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { s } from "./style";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={s.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+//  <View style={s.container}>
+//    <Text>Open up App.tsx to start working on your app!</Text>
+//    <StatusBar style="auto" />
+//  </View>;
 
 // const styles = StyleSheet.create({
 //   container: {
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { s } from "../style";
+
+export default function HomeScreen() {
+  return (
+    <View style={s.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
