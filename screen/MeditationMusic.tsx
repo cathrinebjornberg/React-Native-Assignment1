3aef94908a1da04f7e1c9c47232b723e01fc9ae4
89catbjo <cathrine.bjornberg@live.se>
2024-09-25 10:10:36 +0200

    play music works

--- screen/MeditationMusic.tsx
+++ screen/MeditationMusic.tsx
@@ -1,13 +1,30 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
+import { Audio } from "expo-av";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 import { RootStackParamList } from "../App";
 
 type Props = NativeStackScreenProps<RootStackParamList, "MeditationMusic">;
 
 export default function MeditationMusic() {
+  const [sound, setSound] = useState<Audio.Sound>();
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/sound/relaxing-meditation-231762.mp3")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Meditations Music</Text>
+      <Text>Meditation Music</Text>
+      <Button title="Play" onPress={playSound} />
     </View>
   );
 }
+
+//https://pixabay.com/sv/music/meditation-andlig-relaxing-meditation-231762/
+
+//https://freemusicarchive.org/music/kirk-osamayo/season-two-yellow/meditation-on-love/
