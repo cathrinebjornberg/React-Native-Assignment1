7d35b948d7bbe8f1b80801e7cb424c557da619b3
89catbjo <cathrine.bjornberg@live.se>
2024-09-25 12:36:01 +0200

    start and stop music working

--- screen/MeditationMusic.tsx
+++ screen/MeditationMusic.tsx
@@ -1,26 +1,50 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Audio } from "expo-av";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 import { RootStackParamList } from "../App";
 
+function useToggle(initialValue: boolean) {
+  const [state, setState] = useState(initialValue);
+
+  const toggle = () => {
+    setState((state) => !state);
+  };
+  return { state, toggle, setState };
+}
 type Props = NativeStackScreenProps<RootStackParamList, "MeditationMusic">;
 
 export default function MeditationMusic() {
   const [sound, setSound] = useState<Audio.Sound>();
+  const { state: isPlaying, setState: setIsPlaying, toggle } = useToggle(false);
 
   async function playSound() {
-    const { sound } = await Audio.Sound.createAsync(
-      require("../assets/sound/relaxing-meditation-231762.mp3")
-    );
-    setSound(sound);
-    await sound.playAsync();
+    if (sound && isPlaying) {
+      await sound.stopAsync();
+      setIsPlaying(false);
+    } else {
+      const { sound } = await Audio.Sound.createAsync(
+        require("../assets/sound/relaxing-meditation-231762.mp3")
+      );
+      setSound(sound);
+      setIsPlaying(true);
+      await sound.playAsync();
+    }
   }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Meditation Music</Text>
-      <Button title="Play" onPress={playSound} />
+      <Button title={isPlaying ? "Stop" : "Play"} onPress={playSound} />
     </View>
   );
 }
