ec24734ce3951e4a45144bc01feea4af53937d36
89catbjo <cathrine.bjornberg@live.se>
2024-10-02 12:16:32 +0200

    animation bg color works

--- screen/MeditationMusic.tsx
+++ screen/MeditationMusic.tsx
@@ -2,7 +2,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Audio } from "expo-av";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Animated, Button, StyleSheet, View } from "react-native";
 import { RootStackParamList } from "../App";
 
 function useToggle(initialValue: boolean) {
@@ -19,19 +19,43 @@ export default function MeditationMusic() {
   const [sound, setSound] = useState<Audio.Sound>();
   const { state: isPlaying, setState: setIsPlaying, toggle } = useToggle(false);
 
+  const [scaleValue] = useState(new Animated.Value(1));
+
   async function playSound() {
     if (sound && isPlaying) {
       await sound.stopAsync();
       setIsPlaying(false);
+      stopAnimation();
     } else {
       const { sound } = await Audio.Sound.createAsync(
         require("../assets/sound/relaxing-meditation-231762.mp3")
       );
       setSound(sound);
       setIsPlaying(true);
+      startAnimation();
       await sound.playAsync();
     }
   }
+
+  const startAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleValue, {
+          toValue: 4,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleValue, {
+          toValue: 1,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  };
+  const stopAnimation = () => {
+    scaleValue.setValue(1);
+  };
 
   useEffect(() => {
     return sound
@@ -44,12 +68,25 @@ export default function MeditationMusic() {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={["rgb(222,150,222)", "rgb(22,250,222)"]}
-        style={styles.background}
-      />
-      <Text>Meditation Music</Text>
+      <Animated.View
+        style={[
+          styles.animatedBackground,
+          { transform: [{ scale: scaleValue }] },
+        ]}
+      >
+        <LinearGradient
+          // Background Linear Gradient
+          colors={[
+            "#FFB3BA",
+            "#FFDFBA",
+            "#FFFFBA",
+            "#BAFFC9",
+            "#BAE1FF",
+            "#DABFFF",
+          ]}
+          style={styles.background}
+        />
+      </Animated.View>
       <Button title={isPlaying ? "Stop" : "Play"} onPress={playSound} />
     </View>
   );
@@ -57,6 +94,13 @@ export default function MeditationMusic() {
 
 const styles = StyleSheet.create({
   background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  animatedBackground: {
     position: "absolute",
     left: 0,
     right: 0,
