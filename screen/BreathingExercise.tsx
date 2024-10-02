6461d98cef11dbe21bc6fb8f37717ee9bc591b1e
89catbjo <cathrine.bjornberg@live.se>
2024-10-02 12:14:17 +0200

    animation bg works

--- screen/BreathingExercise.tsx
+++ screen/BreathingExercise.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import * as Haptics from "expo-haptics";
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, StyleSheet, Text, View } from "react-native";
+import { Animated, Button, StyleSheet, Text, View } from "react-native";
 import { RootStackParamList } from "../App";
 
 type Props = NativeStackScreenProps<RootStackParamList, "BreathingExercise">;
@@ -9,6 +9,7 @@ type Props = NativeStackScreenProps<RootStackParamList, "BreathingExercise">;
 export default function BreathingExercise({ navigation }: Props) {
   const animation = useRef(new Animated.Value(1)).current;
   const [breathPhase, setBreathPhase] = useState("Andas in");
+  const [isRunning, setIsRunning] = useState(false);
 
   const breathTimes = {
     inhale: 4000,
@@ -18,67 +19,70 @@ export default function BreathingExercise({ navigation }: Props) {
   };
 
   useEffect(() => {
-    const phases = ["Andas in", "Håll andan", "Andas ut", "Håll andan"];
-    let cycleIndex = 0;
+    if (isRunning) {
+      const phases = ["Andas in", "Håll andan", "Andas ut", "Håll andan"];
+      let cycleIndex = 0;
 
-    const startBreathingCycle = () => {
-      setBreathPhase(phases[cycleIndex]);
+      const startBreathingCycle = () => {
+        setBreathPhase(phases[cycleIndex]);
 
-      // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+        switch (cycleIndex) {
+          case 0:
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+            animation.setValue(1);
+            animateBall(1, 2, breathTimes.inhale, () => {
+              cycleIndex++;
+              setBreathPhase("Håll andan");
+              setTimeout(() => {
+                cycleIndex++;
+                startBreathingCycle();
+              }, breathTimes.holdInhale);
+            });
+            break;
 
-      switch (cycleIndex) {
-        case 0:
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-
-          animation.setValue(1);
-          animateBall(1, 2, breathTimes.inhale, () => {
-            cycleIndex++;
-            setBreathPhase("Håll andan");
+          case 1:
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
             setTimeout(() => {
               cycleIndex++;
-              startBreathingCycle();
+              setBreathPhase("Andas ut");
+              animateBall(2, 1, breathTimes.exhale, () => {
+                cycleIndex++;
+                startBreathingCycle();
+              });
             }, breathTimes.holdInhale);
-          });
-          break;
+            break;
 
-        case 1:
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-          setTimeout(() => {
-            cycleIndex++;
-            setBreathPhase("Andas ut");
+          case 2:
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
             animateBall(2, 1, breathTimes.exhale, () => {
               cycleIndex++;
-              startBreathingCycle();
+              setBreathPhase("Håll andan");
+              setTimeout(() => {
+                startBreathingCycle();
+              }, breathTimes.holdExhale);
             });
-          }, breathTimes.holdInhale);
-          break;
+            break;
 
-        case 2:
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-          animateBall(2, 1, breathTimes.exhale, () => {
-            cycleIndex++;
-            setBreathPhase("Håll andan");
+          case 3:
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
             setTimeout(() => {
+              cycleIndex = 0;
+              setBreathPhase("Andas in");
               startBreathingCycle();
             }, breathTimes.holdExhale);
-          });
-          break;
+            break;
+        }
+      };
 
-        case 3:
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-          setTimeout(() => {
-            cycleIndex = 0;
-            setBreathPhase("Andas in");
-            startBreathingCycle();
-          }, breathTimes.holdExhale);
-          break;
-      }
+      startBreathingCycle();
+    } else {
+      animation.stopAnimation();
+    }
+
+    return () => {
+      animation.stopAnimation();
     };
-
-    startBreathingCycle();
-
-    return () => {};
-  }, []);
+  }, [isRunning]);
 
   const animateBall = (
     fromSize: number,
@@ -105,6 +109,10 @@ export default function BreathingExercise({ navigation }: Props) {
       <Text style={styles.breathText}>{breathPhase}</Text>
       <Animated.View
         style={[styles.ball, { width: ballSize, height: ballSize }]}
+      />
+      <Button
+        title={isRunning ? "Stop" : "Start"}
+        onPress={() => setIsRunning(!isRunning)}
       />
     </View>
   );
